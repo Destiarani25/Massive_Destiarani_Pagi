f8f86f27766e8304c5bba02fc8d20ce6e99fa155
Destiarani25 <114555165+Destiarani25@users.noreply.github.com>
2023-06-09 22:38:09 +0700

    Add files via upload

---
+++ tugas-react/src/component/Iklan.js
@@ -0,0 +1,31 @@
+function Iklan () {
+    return (
+
+<div class="relative isolate overflow-hidden bg-gray-900 py-17 sm:py-20">
+  <img src="photo3.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"/>
+  <div class="hidden sm:absolute sm:-top-15 sm:right-1/2 sm:-z-15 sm:mr-15 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true" >
+    <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style={{"clip-path": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
+  </div>
+  <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
+    <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style={{"clip-path": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}}></div>
+  </div>
+  <div class="mx-auto max-w-15x2 text-center px-30 lg:px-20">
+    <div class="mx-auto max-w-10x2 lg:mx-20">
+      <h2 class="text-4xl font-bold tracking-tight text-black sm:text-6xl">Hello, My Name is Destiarani</h2>
+      <p class="mt-6 text-lg leading-8 text-grey">A girl who has a hobby of fangirling likes everything related to kpop. I like NCT and all the members in NCT, especially Jisung. Love the beach and ice cream. Moodyan but likes to write and read novels.</p>
+    </div>
+    <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+      
+      <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+
+
+     
+           
+          </dl>
+    </div>
+  </div>
+</div>
+
+)
+}
+export default Iklan;
